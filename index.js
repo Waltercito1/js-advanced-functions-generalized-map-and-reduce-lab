3034645d860606169beebcae9276c5360a165fa9
Walter Aab <aabwalter@yahoo.com>
2021-05-21 16:15:33 -0500

    commit tree 6482f79fc932038b7be35a51eef87a1748a891d9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+
+//Map(src,function(a){ return a * -1});
+
+function map(src, callback) {
+    const r = [];
+
+    for (let i = 0; i < src.length; i++) {
+        const token = src[i];
+        r.push(callback(token));
+    }  
+    return r;
+}
+
+function reduce(src, callback, startValue) {
+    let total;
+    if (startValue) {
+        total = startValue;
+        for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+        }
+        return total;
+    } else {
+        total = src[0];
+        for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+        }
+        return total;
+    }
+}
